614897154e653eeb9633164f3742273f094f6b50
Andy Ogier <anogier@microsoft.com>
2023-03-27 14:36:04 -0700

    rename

---
+++ main.go
@@ -0,0 +1,32 @@
+package healthcheckproxy
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func start(port string, appName string, query string) {
+	http.HandleFunc("/health", customHandler(appName, query))
+	http.ListenAndServe(port, nil)
+}
+
+func customHandler(appName string, query string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		response, err := http.Get("http://healthcheck-svc.healthcheck.svc.cluster.local/" + appName + "?" + query)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer response.Body.Close()
+
+		bodyBytes, err := io.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.WriteHeader(response.StatusCode)
+		w.Write(bodyBytes)
+	}
+}
